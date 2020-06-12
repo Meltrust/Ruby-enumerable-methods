ee2377f183e6f8a71ae265778c4aeaf5923b8861
Meltrust <original.mtapia@outlook.com>
2020-06-12 06:12:27 -0500

    add my_any method, add test file

--- enumerables.rb
+++ enumerables.rb
@@ -50,31 +50,13 @@ module Enumerable
     end
     true
   end
+
+  # my_any method
+  def my_any?(par = nil) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    if block_given? then my_each { |tf| return true if yield(tf) || tf.nil? }
+    elsif par.is_a?(Regexp) then my_each { |tf| return true if par === tf } # rubocop:disable Style/CaseEquality
+    else my_each { |tf| return true unless tf == false || tf.nil? }
+    end
+    false
+  end
 end
-# testing my_each
-puts 'Testing my each against [1, 2, 3]'
-[1, 2, 3].my_each do |n|
-  puts n
-end
-
-# testing my_each_index
-puts 'Testing my each against %w[cat dog shark]'
-%w[cat dog shark].my_each_with_index do |idx, each_idx|
-  p "item: #{idx} index: #{each_idx}"
-end
-
-# testing my_select
-puts 'Testing my select against [1, 2, 3, 4, 5]=> select even numbers'
-[1, 2, 3, 4, 5].my_select do |num|
-  puts num if num.even?
-end
-
-# testing my_all
-puts 'Testing my all? against %w[antd befsd catd] all elements >= lenght 4?'
-puts(%w[antd befsd catd].my_all? { |word| word.length >= 4 })
-
-puts 'Testing my all? against %w[antbea beatr bea] includes pattern /bea/ ?'
-puts(%w[antea batr bea].my_all?(/bea/))
-
-puts 'Testing my all? against [3, 3, nil, "4", 2] no element is false or nil?'
-puts([3, 3, false, '4', 2].my_all?)
