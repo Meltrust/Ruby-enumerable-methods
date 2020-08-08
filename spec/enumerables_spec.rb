54bc3f38ef62b16728e33ac33cf032fed0b0e92a
Meltrust <original.mtapia@outlook.com>
2020-08-08 01:12:28 -0500

    added 2 more tests

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,7 +2,8 @@
 
 require './enumerables.rb'
 
-RSpec.describe Enumerable do
+describe Enumerable do
+  # main testing variables
   let(:arr1) { %w[dog bear cat] }
   let(:arr2) { [true, nil, 90] }
   let(:arr3) { [1, 2, 3, 4, 5] }
@@ -10,7 +11,19 @@ RSpec.describe Enumerable do
 
   describe '#my_each' do
     it 'every element of an array is passed to a block by this method' do
-      expect(arr1.each { |a| a }).to eql(%w[dog bear cat])
+      expect(arr1.each { |a| }).to eql(%w[dog bear cat])
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'every element of an array is passed to a block with its index by this method' do
+      expect(arr1.my_each_with_index { |a, i| }).to eql(%w[dog bear cat])
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns an array with all the elements that are true to the given block condition' do
+      expect(arr3.my_select { |i| i % 3 == 0 }).to eql([3])
     end
   end
 end
